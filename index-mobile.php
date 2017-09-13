ffe000b88717e33ba73efde8a16cc41fd8ebd870
Oscar del Olmo <oscar@wm4.mx>
2017-09-12 19:01:53 -0500

    mobile header fix
    relocation of the header in index-mobile.php to better constrain to the slideout UIX intended

--- index-mobile.php
+++ index-mobile.php
@@ -6,54 +6,54 @@ include 'header-mobile.php';
 ?>
 
 <section id="slideout">
-    <header class="centered-navigation" role="banner">
-        <div class="centered-navigation-wrapper">
-            <nav role="navigation">
-                <ul id="js-centered-navigation-menu" class="centered-navigation-menu show">
-                    <li class="nav-link">
-                        <a href="https://www.google.com.mx/maps/place/Vito+Alessio+Robles+101,+Agr%C3%ADcola,+Chimalistac,+01030+Ciudad+de+M%C3%A9xico,+CDMX/@19.3511402,-99.1850127,17z/data=!4m5!3m4!1s0x85d1fff0e0e33ca7:0x4355e17e0a3580dd!8m2!3d19.351135!4d-99.182824?hl=en"><span class="fa fa-map-marker"></span></a>
-                    </li>
-                    <li class="nav-link"><a href="javascript:void(0)"><span class="fa fa-phone"></span></a></li>
-                    <li class="nav-link"><a href="mailto:contacto@wm4.mx"><span class="fa fa-envelope"></span></a></li>
-                    <li class="nav-link logo">
-                        <a class="logo">
-                            <div id="contacto_head"><span>contacto</span></div>
-                        </a>
-                    </li>
-                    <li class="nav-link"><a href="https://twitter.com/wm4mx"><span class="fa fa-twitter"></span></a></li>
-                    <li class="nav-link"><a href="https://www.facebook.com/webmedia4/?fref=ts"><span class="fa fa-facebook"></span></a></li>
-                    <li class="nav-link"><a href="https://www.instagram.com/wm4agency/"><span class="fa fa-instagram"></span></a></li>
-                </ul>
-            </nav>
-        </div>
-
-    </header><!--/.container-->
     <main id="home" class="mobile">
-            <div class="bloque_wrapper">
-                <section class="bloque">
-                    <div class="row">
-                        <figure class="main_logo">
-                            <img src="img/wm4.png" class="modal-trigger">
-                        </figure>
-                    </div>
-                    <div id="cycle" class="row">
-                        <h3 id="cycling">&nbsp;</h3>
-                    </div>
-                    <div id="unidades">
-                        <div class="row">
-                            <article class="span-4 unidades">
-                                <h1 class="slideout-trigger" data-slideout-menu="mkt-digital">Marketing<br /> Digital</h1>
-                            </article>
-                            <article class="span-4 unidades">
-                                <h1 class="slideout-trigger" data-slideout-menu="mkt-politico">Marketing<br /> Político</h1>
-                            </article>
-                            <article class="span-4 omega unidades">
-                                <h1 class="slideout-trigger" data-slideout-menu="mkt-deportivo">Marketing<br /> Deportivo</h1>
-                            </article>
-                        </div>
-                    </div>
-                </section>
+        <header class="centered-navigation" role="banner">
+            <div class="centered-navigation-wrapper">
+                <nav role="navigation">
+                    <ul id="js-centered-navigation-menu" class="centered-navigation-menu show">
+                        <li class="nav-link">
+                            <a href="https://www.google.com.mx/maps/place/Vito+Alessio+Robles+101,+Agr%C3%ADcola,+Chimalistac,+01030+Ciudad+de+M%C3%A9xico,+CDMX/@19.3511402,-99.1850127,17z/data=!4m5!3m4!1s0x85d1fff0e0e33ca7:0x4355e17e0a3580dd!8m2!3d19.351135!4d-99.182824?hl=en"><span class="fa fa-map-marker"></span></a>
+                        </li>
+                        <li class="nav-link"><a href="javascript:void(0)"><span class="fa fa-phone"></span></a></li>
+                        <li class="nav-link"><a href="mailto:contacto@wm4.mx"><span class="fa fa-envelope"></span></a></li>
+                        <li class="nav-link logo">
+                            <a class="logo">
+                                <div id="contacto_head"><span>contacto</span></div>
+                            </a>
+                        </li>
+                        <li class="nav-link"><a href="https://twitter.com/wm4mx"><span class="fa fa-twitter"></span></a></li>
+                        <li class="nav-link"><a href="https://www.facebook.com/webmedia4/?fref=ts"><span class="fa fa-facebook"></span></a></li>
+                        <li class="nav-link"><a href="https://www.instagram.com/wm4agency/"><span class="fa fa-instagram"></span></a></li>
+                    </ul>
+                </nav>
             </div>
+        </header><!--/.container-->
+        
+        <div class="bloque_wrapper">
+            <section class="bloque">
+                <div class="row">
+                    <figure class="main_logo">
+                        <img src="img/wm4.png" class="modal-trigger">
+                    </figure>
+                </div>
+                <div id="cycle" class="row">
+                    <h3 id="cycling">&nbsp;</h3>
+                </div>
+                <div id="unidades">
+                    <div class="row">
+                        <article class="span-4 unidades">
+                            <h1 class="slideout-trigger" data-slideout-menu="mkt-digital">Marketing<br /> Digital</h1>
+                        </article>
+                        <article class="span-4 unidades">
+                            <h1 class="slideout-trigger" data-slideout-menu="mkt-politico">Marketing<br /> Político</h1>
+                        </article>
+                        <article class="span-4 omega unidades">
+                            <h1 class="slideout-trigger" data-slideout-menu="mkt-deportivo">Marketing<br /> Deportivo</h1>
+                        </article>
+                    </div>
+                </div>
+            </section>
+        </div>
     </main>
 </section>
 
