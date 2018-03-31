4c1c325e372c4bc7914c3e08890febcbe39fb30e
mono-entrenado <oscar@wm4.mx>
2018-03-31 15:03:09 -0600

    "valores" unslider style
    css styling of unslider element "valores" in "modelo" home screen

--- php/partials/home_modelo.php
+++ php/partials/home_modelo.php
@@ -12,24 +12,28 @@
         <p></p>
     </article>
 </main>
-<aside id="valores" class="unslider" data-unslider_selectors='{"container": "dl:first","slides":"article"}'>
-    <h2 class="hexatitle left">Trabajamos con valor</h2>
-    <dl>
-        <article>
-            <dt>Talento</dt>
-            <dd>Nuestro equipo está formado por una constelación de individuos con experiencias y capacidades que sobresalen y se complementan</dd>
-        </article>
-        <article>
-            <dt>Eficacia</dt>
-            <dd>Trabajamos todos los días por construir resultados tangibles en la operación de nuestros clientes, que contribuyen a generar ingreso.</dd></article>
-        <article>
-            <dt>Pasión</dt>
-            <dd>Hacemos lo que amamos y amamos lo que hacemos, por eso lo hacemos con el corazón y la cabeza, y eso se nota en los resultados</dd></article>
-        <article>
-            <dt>Compromiso</dt>
-            <dd></dd>
-        </article>
-    </dl>
+<aside id="valores">
+    <h2 class="hexatitle left shift-1 span-3">Trabajamos con valor</h2>
+    <div class="span-8 omega unslider" data-unslider_selectors='{"container":"dl:first","slides":"article"}'>
+        <div class="unslider-wrapp">
+            <dl>
+                <article>
+                    <dt>Talento</dt>
+                    <dd>Nuestro equipo está formado por una constelación de individuos con experiencias y capacidades que sobresalen y se complementan</dd>
+                </article>
+                <article>
+                    <dt>Eficacia</dt>
+                    <dd>Trabajamos todos los días por construir resultados tangibles en la operación de nuestros clientes, que contribuyen a generar ingreso.</dd></article>
+                <article>
+                    <dt>Pasión</dt>
+                    <dd>Hacemos lo que amamos y amamos lo que hacemos, por eso lo hacemos con el corazón y la cabeza, y eso se nota en los resultados</dd></article>
+                <article>
+                    <dt>Compromiso</dt>
+                    <dd></dd>
+                </article>
+            </dl>
+        </div>
+    </div>
     <!--<ul>
         <li>Talento</li>
             <li>Eficacia</li>
