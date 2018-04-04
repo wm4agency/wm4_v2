1723876b7bbce55293127961cee3fc0bc0022968
Oscar del Olmo <oscar@wm4.mx>
2018-04-04 17:43:40 -0500

    google tag manager
    sustitution of analytics.js implementation for a more modern, future proof solution tightly integrated with the full google suite ecosystem

--- php/partials/index-desktop.php
+++ php/partials/index-desktop.php
@@ -5,6 +5,6 @@ $target = $d.'.php';
 !$d && $target='home.php';
 if (! @include_once($target)){include_once("404.php");}
 if (! @include_once('php/components/modal_contacto.php')){echo "couldn't include modal";}
-if (! @include_once('footer.php')){echo "couldn't include modal";}
+if (! @include_once('footer.php')){echo "footer not loaded";}
 
 ?>
--- php/partials/m-head.php
+++ php/partials/m-head.php
@@ -39,17 +39,4 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
 </script>
 </head>
     <body class="mobile">
-        <?php  if ($header == 'full'): ?>
-        <header class="centered-navigation" role="banner">
-            <div class="centered-navigation-wrapper">
-                <?php include 'nav_contactPoints.php'; ?>
-            </div>
-
-        </header><!--/.container-->
-
-        <?php elseif ($header == 'minimal'): ?>
-        <header class="minimal-navigation fullwidth">
-            <div class="container"><figure class="logo"><a href="index.php"><img src="img/wm4.png"></a></figure></div>
-            <div class="hexa-tip-right"><figure>&nbsp;</figure></div>
-        </header>
-        <?php endif; ?>
+        <?php if (! @include_once('php/components/m-slideMenu.php')){die "couldn't include sliding menu";} ?>
