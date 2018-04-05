0e4c896633b22e498e626e508bc67161def84a39
Oscar del Olmo <oscar@wm4.mx>
2018-04-05 18:16:29 -0500

    home sections

--- php/partials/m-home-main.php
+++ php/partials/m-home-main.php
@@ -1,4 +1,4 @@
-<section class="fullwidth">
+<div class="fullwidth">
     <div class="fullwidth">
         <figure class="main_logo">
             <img src="img/wm4.png" class="modal-trigger">
@@ -19,7 +19,7 @@
         </article>
     </div>
     <div class="bloque monolito-wrapper animated fadeInRight"><?php include 'monolito_der.php'; ?></div>
-</section>
+</div>
 <dl itemprop="hasOfferCatalog" itemscope itemtype="http://schema.org/OfferCatalog" id="unidades" class="fullwidth animated fadeInDown">
     <h1>Marketing Especializado</h1>
     <div>
