dd5a825e223a2e0de72595436ec75cffd21acd3a
Oscar del Olmo <oscar@wm4.mx>
2018-04-10 17:20:47 -0500

    "modelo" home styling + angled mixin
    home section "modelo" css styles. First attempt to new angled_down mixin to render full arrowed display elements

--- php/partials/m-home-modelo.php
+++ php/partials/m-home-modelo.php
@@ -1,18 +1,18 @@
 <!-- HOME "MODELO" SECTION CONTENTS -->
 
-<article class="span-4 animate fadeInDown" >
+<article id="estrategias" class="span-4 animate fadeInDown" >
     <h1 class="hexatitle centered ">Creamos estrategias</h1>
     <p>Planeamos el trabajo a partir de objetivos de negocio alcanzables y medibles, construimos un plan de trabajo accionable y definimos los indicadores que permitirán evaluar y optimizar el trabajo en cada etapa del proyecto.</p>
 </article>
-<article class="span-4 animate fadeInDown">
+<article id="proyectos" class="span-4 animate fadeInDown">
     <h1 class="hexatitle centered">Desarrollamos proyectos</h1>
     <p>Trabajamos bajo procesos ágiles y consistentes para entregar resultados concretos que contribuyen a la rentabilidad y el desarrollo de la marca, que medimos con herramientas e indicadores adecuados para optimizar constantemente.</p>
 </article>
-<article class="span-4 omega animate fadeInDown">
+<article id="resultados" class="span-4 omega animate fadeInDown">
     <h1 class="hexatitle centered">Entregamos resultados</h1>
     <p>Aplicamos experiencia, creatividad y talento a entregar ROI en cada proyecto, produciendo impactos tangibles en las metas de negocio de nuestros clientes, que provocan los retornos de inversión mas competitivos en la industria.</p>
 </article>
-<article id="valores animate fadeInDown">
+<article id="valores" id="valores animate fadeInDown">
     <h1 class="hexatitle centered shift-1 span-3">Trabajamos con valor</h1>
     <div class="span-8 omega unslider-wrapp">
         <div class="unslider" data-unslider_options='{"speed":1000,"autoplay":true,"delay":5000,"animation":"horizontal","selectors":{"container":"dl:first","slides":"div"},"infinite":true}'>
